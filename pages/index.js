bb6731f8b8292bb94b056fb8894d8cf07f06f1f3
pasan samarakoon <pasansmpl@gmail.com>
2022-11-25 05:36:48 +0530

    Initial project
    Initial project

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I am Sitecore® 9.1 Certified Platform Associate Developer.
+           Currently working exclusively with Sitecore. 
+           I have more than 8 years of Sitecore development 
+           experience and over 10 years of experience in Microsoft technologies.
+            Which mainly includes Asp.net and MVC.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
